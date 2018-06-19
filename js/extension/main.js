cff8b5abcab69fda621e4dd463a9149850e89e9e
AntonioXIII <aj72325@gmail.com>
2018-06-18 22:01:49 -0600

    Add Timer Started
    The basic structural layout of the timer wrapper has been implemented,
    and an on click event handler for the add timer button adds a
    non-working timer's structure.

--- js/extension/main.js
+++ js/extension/main.js
@@ -3,6 +3,37 @@
  */
 $(document).ready(function(){
   
-  $('#changeColor').attr('random', 'ARANDOMVALUE');
+  var newTimerHTML = '<section class="timer">' + 
+                      '<header class="timerHeader">' + 
+                      '<h2>Title</h2>' +
+                      '</header>' +
+                      '<section class="timerInfo">' +
+                      '<section class="timerEntry">' + 
+                      '<input type="text" class="timerHour" value="00" />' +
+                      '<span class="timerColon">:</span>' +
+                      '<input type="text" class="timerMinute" value="00" />' +
+                      '<span class="timerColon">:</span>' + 
+                      '<input type="text" class="timerSecond" value="00" />' +
+                      '</section>' + 
+                      '<section class="timerButtons">' +
+                      '<button class="playPauseBtn">Play</button>' +
+                      '<button class="resetBtn">Reset</button>' +
+                      '<button class="deleteBtn">Delete</button>' +
+                      '</section>' + 
+                      '<section class="timerBillable">' +
+                      '<input type="checkbox" class="nonbillableCb" />' + 
+                      '</section>' +
+                      '<section class="ticketInfo">' + 
+                      '<label class="ticketNum">#</label>' +
+                      '<input type="textarea" class="ticketDesc"/>' + 
+                      '<button class="timerSubmit">Submit</button>' + 
+                      '</section>' +
+                      '</section>';
+  
+  $('#addTimer').click(function(){
+    
+    $('#app-content').append(newTimerHTML);
+    
+  });
   
 });
